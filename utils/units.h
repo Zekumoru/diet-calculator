d0ab22158c2783e8cad683a4e28a5e4023c1201b
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-11-07 15:39:09 +0100

    Create sex, weight, and height units enum

---
+++ utils/units.h
@@ -0,0 +1,19 @@
+#ifndef UNITS_H
+#define UNITS_H
+
+enum Sex {
+    MALE,
+    FEMALE
+};
+
+enum WeightUnit {
+    KILOGRAMS,
+    POUNDS
+};
+
+enum HeightUnit {
+    CENTIMETERS,
+    FEET
+};
+
+#endif // UNITS_H
